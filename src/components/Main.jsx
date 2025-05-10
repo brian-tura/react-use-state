eab31e2fd7b4f2aea50dce399e3ac46d91c3999f
brian-tura <b.tura2002@gmail.com>
2025-05-10 14:36:01 +0200

    [FEAT] Reactive buttons and card added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,47 @@
 import React from 'react'
-import ButtonList from './ButtonList'
 import languages from '../../languages';
 import { useState } from 'react'
 
 const Main = () => {
-    const [selectedLanguage, setActiveLanguage] = useState(languages[0]);
+    const [selectedLanguage, setActiveLanguage] = useState(null);
+
+    const renderSelectedLanguage = () => {
+        if (selectedLanguage === null) {
+            return <h3>Nessun linguaggio selezionato</h3>;
+        }
+        const { title, language } = selectedLanguage;
+
+        return (
+            <>
+                <h5>{selectedLanguage.title}</h5>
+                <p>{selectedLanguage.description}</p>
+            </>
+        )
+    }
+
+
 
     return (
         <>
             <main>
-                <div>
-                    <ButtonList />
-                </div>
-                <div>
-                    <h5>{selectedLanguage.title}</h5>
-                    <p>{selectedLanguage.description}</p>
+                <div className='container'>
+                    <div className='content'>
+                        {languages.map((language) => (
+                            <button
+                                onClick={() => setActiveLanguage(language)}
+                                className='btn btn-primary'
+                                key={language.id}
+                            >
+                                {language.title}
+                            </button>
+                        ))}
+                    </div>
+                    <div className='content'>
+                        <div className='card'>
+                            {renderSelectedLanguage()}
+                        </div>
+                    </div>
+
                 </div>
             </main>
         </>
