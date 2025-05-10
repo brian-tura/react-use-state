f2acc8f66c0d72d19249b3dec7884ba33bc65e46
brian-tura <b.tura2002@gmail.com>
2025-05-10 13:48:58 +0200

    [FEAT] Base structure done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import './App.css'
 import Main from './components/Main'
 
+
 function App() {
   return (
     <>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ButtonList from './ButtonList'
+import languages from '../../languages';
+import { useState } from 'react'
 
 const Main = () => {
+    const [selectedLanguage, setActiveLanguage] = useState(languages[0]);
+
     return (
         <>
             <main>
-                <ButtonList />
+                <div>
+                    <ButtonList />
+                </div>
+                <div>
+                    <h5>{selectedLanguage.title}</h5>
+                    <p>{selectedLanguage.description}</p>
+                </div>
             </main>
         </>
     )
